84023bc854d782f37f38e36a1a4043c5ba5530f4
David Patuwo <dpatuwo@kent.edu>
2016-04-29 12:29:51 -0400

    SQL files now working

--- lib/forum_schema.sql
+++ lib/forum_schema.sql
@@ -2,31 +2,22 @@ DROP TABLE IF EXISTS topics;
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS posts;
 DROP TABLE IF EXISTS comments;
+CREATE TABLE topics(id SERIAL PRIMARY KEY, name VARCHAR(30), relation INTEGER, description TEXT);
 
-CREATE TABLE topics (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR,
-  relation INTEGER,
-  description VARCHAR
-);
-
-CREATE TABLE users (
-  id SERIAL PRIMARY KEY,
-  name VARCHAR,
-  access INTEGER, # 0 - Regular user  1 - Moderator  2 - Admin
-);
+ -- 0 - Regular user  1 - Moderator  2 - Admin
+CREATE TABLE users (id SERIAL PRIMARY KEY, name VARCHAR(30), access INTEGER);
 
 CREATE TABLE posts (
   id SERIAL PRIMARY KEY,
-  name VARCHAR,
-  body VARCHAR,
+  name VARCHAR(30),
+  body TEXT,
   topic_id INTEGER,
   user_id INTEGER
 );
 
 CREATE TABLE comments (
   id SERIAL PRIMARY KEY,
-  body VARCHAR,
+  body TEXT,
   user_id INTEGER,
   post_id INTEGER,
   relation INTEGER
--- lib/forum_seeds.sql
+++ lib/forum_seeds.sql
@@ -1,13 +1,13 @@
-DROP TABLE IF EXISTS topics
-DROP TABLE IF EXISTS users
-DROP TABLE IF EXISTS posts
-DROP TABLE IF EXISTS comments
+--DROP TABLE IF EXISTS topics;
+--DROP TABLE IF EXISTS users;
+--DROP TABLE IF EXISTS posts;
+--DROP TABLE IF EXISTS comments;
 
-CREATE TABLE topics
-CREATE TABLE users
-CREATE TABLE posts
-CREATE TABLE comments
+--CREATE TABLE topics
+--CREATE TABLE users
+--CREATE TABLE posts
+--CREATE TABLE comments
 
-INSERT INTO topics (id, name, relation, description) VALUES (1, 'Random', 0, 'Put anything you want in here!')
-INSERT INTO users (id, name, access) VALUES (1, 'David', 2)
-INSERT INTO posts (id, name, body, topic_id, user_id) VALUES (1, 'Welcome to the Random board!', 'Want to talk about something?  Just post it here, but please keep your posts PG or else your post will be taken down', 1, 1)
+INSERT INTO topics (name, relation, description) VALUES ('Random', 0, 'Put anything you want in here!');
+INSERT INTO users (name, access) VALUES ('David', 2);
+INSERT INTO posts (name, body, topic_id, user_id) VALUES ('Welcome to the Random board!', 'Want to talk about something?  Just post it here, but please keep your posts PG or else your post will be taken down', 1, 1);
